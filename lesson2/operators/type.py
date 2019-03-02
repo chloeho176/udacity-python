188bca994a457cd6d8c06a423ca99ed450240cac
chloeho176 <bangngan1995@gmail.com>
2019-03-02 13:53:18 -0800

    type conversions

--- lesson2/operators/type.py
+++ lesson2/operators/type.py
@@ -9,7 +9,11 @@ def printFunction():
     print(type(2))
     print(type(2.))
     print(type(2.0))
-
+    
+    # comments - type conversions
+    print(int(16/4))
+    print(float(15/4))
+    print(int(15/4))
 
 if __name__ == "__main__": 
     printFunction() 

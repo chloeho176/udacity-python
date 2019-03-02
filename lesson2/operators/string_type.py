a6a2ef630d2ae158ecb04a261ed0405cbe391917
chloeho176 <bangngan1995@gmail.com>
2019-03-02 14:44:59 -0800

    address_format

---
+++ lesson2/operators/string_type.py
@@ -0,0 +1,12 @@
+def printFunction(): 
+
+    house_number = 13
+    street_name = "cresent"
+    town_name = "bermont"
+    print(type(house_number))
+
+    address = str(house_number) + " " + street_name + ", " + town_name
+    print(address)
+
+if __name__ == "__main__":
+    printFunction() 

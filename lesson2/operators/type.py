b87be18acced572044394f3cc41582606021955d
chloeho176 <bangngan1995@gmail.com>
2019-03-02 13:48:51 -0800

    Added type

---
+++ lesson2/operators/type.py
@@ -0,0 +1,12 @@
+def printFunction(): 
+    print(type("Hello World"))
+    print(type(5+3))
+    print(type(5/3))
+    print(type(5*3))
+    print(type(5-3))
+    print(type(2**2))
+    print(type(2//2))
+
+
+if __name__ == "__main__": 
+    printFunction() 

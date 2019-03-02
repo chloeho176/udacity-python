996bdb014ca53a89e7e76c73d599d81c09434445
chloeho176 <bangngan1995@gmail.com>
2019-03-02 14:13:55 -0800

    comparision

---
+++ lesson2/operators/comparision.py
@@ -0,0 +1,9 @@
+def printFunction(): 
+
+    # comments - comparision conversions
+    age = 14
+    not_teen = not (age > 12 and age < 20)
+    print(not_teen)
+
+if __name__ == "__main__": 
+    printFunction() 

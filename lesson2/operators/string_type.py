d60260f0f35e327dc7bea120f9bbc76d73a827b4
chloeho176 <bangngan1995@gmail.com>
2019-03-02 14:53:06 -0800

    find

--- lesson2/operators/string_type.py
+++ lesson2/operators/string_type.py
@@ -8,5 +8,10 @@ def printFunction():
     address = str(house_number) + " " + street_name + ", " + town_name
     print(address)
 
+    my_string = "Chloe Ho"
+                # 01234567
+    print(my_string.find("o"))
+    print(my_string.count("o"))
+
 if __name__ == "__main__":
     printFunction() 

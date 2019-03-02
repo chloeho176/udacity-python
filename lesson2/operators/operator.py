9b9c0338eff94758afc58e8cd2c071c148b61ed5
Chloe <bangngan1995@gmail.com>
2019-03-02 13:13:28 -0800

    My first program in python

---
+++ lesson2/operators/operator.py
@@ -0,0 +1,12 @@
+def printFunction(): 
+    print "Hello World" 
+    print(5+3)
+    print(5/3)
+    print(5*3)
+    print(5-3)
+    print(2**2)
+    print(2//2)
+
+
+if __name__ == "__main__": 
+    printFunction() 

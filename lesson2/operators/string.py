c5b849bd5a176fe51e87aab8ed2c7033b4feaf60
chloeho176 <bangngan1995@gmail.com>
2019-03-02 14:22:18 -0800

    string addition

---
+++ lesson2/operators/string.py
@@ -0,0 +1,10 @@
+def printFunction(): 
+
+    # comments - string conversions
+    first_word = "hello"
+    second_word = "chloe"
+
+    print(first_word + second_word)
+
+if __name__ == "__main__": 
+    printFunction() 
